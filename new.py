819753d225898f04750d4ba60def070347e2c87e
lalaxd09 <taniaro37@gmail.com>
2022-05-01 00:39:50 -0500

    Initial commit

--- new.py
+++
@@ -1,7 +0,0 @@
-import my_networkx as my_nx
-edge_weights = nx.get_edge_attributes(G,'w')
-curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
-straight_edge_labels = {edge: edge_weights[edge] for edge in straight_edges}
-my_nx.my_draw_networkx_edge_labels(G, pos, ax=ax, edge_labels=curved_edge_labels,rotate=False,rad = arc_rad)
-nx.draw_networkx_edge_labels(G, pos, ax=ax, edge_labels=straight_edge_labels,rotate=False)
-fig.savefig("3.png", bbox_inches='tight',pad_inches=0)
